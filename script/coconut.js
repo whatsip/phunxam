1d392ea0d5381d0128df6c876455b6cb3a02b4c0
khanhlam <khanhlam.photo@gmail.com>
2026-03-24 22:02:37 +0700

    Add coconut.js to display GIFs and remove after 4s

---
+++ script/coconut.js
@@ -0,0 +1,31 @@
+(() => {
+
+  const left =
+    document.getElementById("coconut-left");
+
+  const right =
+    document.getElementById("coconut-right");
+
+
+  if (!left || !right) return;
+
+
+  /* Add GIF images */
+
+  left.innerHTML =
+    '<img src="asset/coconut-left.gif">';
+
+  right.innerHTML =
+    '<img src="asset/coconut-right.gif">';
+
+
+  /* Remove after 4 seconds */
+
+  setTimeout(() => {
+
+    left.remove();
+    right.remove();
+
+  }, 4000);
+
+})();
